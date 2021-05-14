b0188c3f302a82cde54de0ba98532831e71d26fa
Enno Boland <g@s01.de>
2021-05-14 11:02:03 +0200

    clients/rainbow: add support for brightness

--- clients/rainbow/src/main.rs
+++ clients/rainbow/src/main.rs
@@ -1,9 +1,9 @@
-use std::net::UdpSocket;
-use std::net::SocketAddr;
-use std::time::Duration;
 use libhexapanel::*;
 use raqote::*;
 use std::f32::consts::PI;
+use std::net::SocketAddr;
+use std::net::UdpSocket;
+use std::time::Duration;
 use structopt::*;
 
 const SIZE: (i32, i32) = (23, 18);
@@ -11,6 +11,10 @@ const SIZE: (i32, i32) = (23, 18);
 #[derive(StructOpt, Debug)]
 #[structopt(name = "rainbow")]
 struct Opt {
+    #[structopt(short, long, default_value = "128")]
+    /// brightness [0-255]
+    brightness: u8,
+
     #[structopt(short, long, default_value = "360")]
     /// interval in milliseconds
     steps: u64,
@@ -22,7 +26,6 @@ struct Opt {
     #[structopt(name = "HOST:PORT")]
     addr: SocketAddr,
 }
-
 
 fn rotate(angle: f32, p: Point, cot: Point) -> Point {
     let angle = 2. * PI * angle;
@@ -40,7 +43,7 @@ fn main() -> Result<()> {
     let mut hexapanel = HexaPanel::<UdpSocket>::connect(opt.addr)?;
     for i in std::iter::repeat(0..opt.steps).flatten() {
         let angle = i as f32 / opt.steps as f32;
-        draw_gradient(angle, &mut dt);
+        draw_gradient(angle, &mut dt, opt.brightness);
         hexapanel.send_frame(&dt)?;
         std::thread::sleep(Duration::from_millis(opt.interval));
     }
@@ -48,55 +51,51 @@ fn main() -> Result<()> {
     unreachable!();
 }
 
-fn draw_gradient(angle: f32, dt: &mut DrawTarget) {
+fn draw_gradient(angle: f32, dt: &mut DrawTarget, brightness: u8) {
     let mut pb = PathBuilder::new();
     pb.rect(0., 0., SIZE.0 as f32, SIZE.1 as f32);
     pb.close();
     let path = pb.finish();
 
-    let center_of_rotation = Point::new(SIZE.0 as f32 / 2., SIZE.1 as f32 / 2.);
+    //let center_of_rotation = Point::new(SIZE.0 as f32 / 2., SIZE.1 as f32 / 2.);
+    let center_of_rotation = Point::new(SIZE.0 as f32 / 2., SIZE.1 as f32 / 1.);
 
     let gradient = Source::new_linear_gradient(
         Gradient {
             stops: vec![
                 GradientStop {
                     position: 0. / 6.,
-                    color: Color::new(0xff, 0xff, 0, 0),
+                    color: Color::new(brightness, brightness, 0, 0),
                 },
                 GradientStop {
                     position: 1. / 6.,
-                    color: Color::new(0xff, 0xff, 0xff, 0),
+                    color: Color::new(brightness, brightness, brightness, 0),
                 },
                 GradientStop {
                     position: 2. / 6.,
-                    color: Color::new(0xff, 0, 0xff, 0),
+                    color: Color::new(brightness, 0, brightness, 0),
                 },
                 GradientStop {
                     position: 3. / 6.,
-                    color: Color::new(0xff, 0, 0xff, 0xff),
+                    color: Color::new(brightness, 0, brightness, brightness),
                 },
                 GradientStop {
                     position: 4. / 6.,
-                    color: Color::new(0xff, 0, 0, 0xff),
+                    color: Color::new(brightness, 0, 0, brightness),
                 },
                 GradientStop {
                     position: 5. / 6.,
-                    color: Color::new(0xff, 0xff, 0, 0xff),
+                    color: Color::new(brightness, brightness, 0, brightness),
                 },
                 GradientStop {
                     position: 6. / 6.,
-                    color: Color::new(0xff, 0xff, 0, 0),
+                    color: Color::new(brightness, brightness, 0, 0),
                 },
             ],
         },
         rotate(angle, Point::new(0., 0.), center_of_rotation),
-        rotate(
-            angle,
-            Point::new(0., SIZE.1 as f32),
-            center_of_rotation,
-        ),
+        rotate(angle, Point::new(0., SIZE.1 as f32), center_of_rotation),
         Spread::Repeat,
     );
     dt.fill(&path, &gradient, &DrawOptions::new());
-
 }
